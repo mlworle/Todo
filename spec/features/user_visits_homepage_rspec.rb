84701ea587281e65aa16a86d2e759fed31ffed6f
mlworle <mlworle@gmail.com>
2015-11-22 12:47:20 -0500

    Allow a user to create a new todo

--- spec/features/user_visits_homepage_rspec.rb
+++ spec/features/user_visits_homepage_rspec.rb
@@ -1,9 +1,15 @@
 require "rails_helper"
+# Follows Upcase Trial Test-Driven Rails with Josh Clayton
 
+# Video 1
 feature "User visits homepage" do
   scenario "successfully" do
     visit root_path
 
     expect(page).to have_css 'h1', text: 'Todos'
   end
+
+# Video 2
+
+feature "
 end
